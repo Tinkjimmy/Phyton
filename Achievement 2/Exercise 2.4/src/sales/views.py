15ec8b41afe7bd994995af3decb9caf87ed1f474
Tinkjimmy <tinkjimmy938@gmail.com>
2024-01-14 18:34:27 +0100

    task 2.4

--- Achievement 2/Exercise 2.4/src/sales/views.py
+++ Achievement 2/Exercise 2.4/src/sales/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'recipes/recipes_home.html')
+    return render(request,'sales/home.html')
+
