cd0b291b1fd0051ef48c86e887e857c3d6246b10
Tinkjimmy <tinkjimmy938@gmail.com>
2024-02-04 12:50:19 +0100

    ok

--- Achievement 2/Exercise 2.4/src/bookstore/settings.py
+++ Achievement 2/Exercise 2.4/src/bookstore/settings.py
@@ -123,6 +123,9 @@ LOGIN_URL='/login/'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+   BASE_DIR / 'static'
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT= BASE_DIR / 'media'
